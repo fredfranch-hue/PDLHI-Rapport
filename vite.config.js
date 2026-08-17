2f39fa4f6c623286eebe9c2deb3b326ab9586e06
Frédéric Franch <fred.franch@yahoo.fr>
2026-08-17 23:53:13 +0200

    Correction start_url et scope PWA

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
         background_color: '#ffffff',
         display: 'standalone',
         orientation: 'portrait',
-        start_url: '/',
+        start_url: '/PDLHI-Rapport/',
         scope: '/',
         lang: 'fr',
         categories: ['productivity'],
